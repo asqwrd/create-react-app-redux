6494c26ede70782a55b4b5b0c86cc3b13963e089
Ajibade Adebayo <aadebayo@webdam.com>
2018-02-27 10:44:36 -0800

    updated to fit my setup

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+import {connectWithLifecycle} from 'react-lifecycle-component/lib'
+
 import {
   increment,
   incrementAsync,
@@ -9,24 +10,31 @@ import {
   decrementAsync
 } from '../../modules/counter'
 
-const Home = props => (
-  <div>
-    <h1>Home</h1>
-    <p>Count: {props.count}</p>
+const Home = (props) =>{
 
-    <p>
-      <button onClick={props.increment} disabled={props.isIncrementing}>Increment</button>
-      <button onClick={props.incrementAsync} disabled={props.isIncrementing}>Increment Async</button>
-    </p>
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Count: {props.count}</p>
 
-    <p>
-      <button onClick={props.decrement} disabled={props.isDecrementing}>Decrement</button>
-      <button onClick={props.decrementAsync} disabled={props.isDecrementing}>Decrement Async</button>
-    </p>
+      <p>
+        <button onClick={props.increment} disabled={props.isIncrementing}>Increment</button>
+        <button onClick={props.incrementAsync} disabled={props.isIncrementing}>Increment Async</button>
+      </p>
 
-    <p><button onClick={() => props.changePage()}>Go to about page via redux</button></p>
-  </div>
-)
+      <p>
+        <button onClick={props.decrement} disabled={props.isDecrementing}>Decrement</button>
+        <button onClick={props.decrementAsync} disabled={props.isDecrementing}>Decrement Async</button>
+      </p>
+
+      <p><button onClick={() => props.changePage()}>Go to about page via redux</button></p>
+    </div>
+  )
+}
+
+const firstLoad = ()=>{
+  console.log('Example of connect lifecycle')
+}
 
 const mapStateToProps = state => ({
   count: state.counter.count,
@@ -34,15 +42,17 @@ const mapStateToProps = state => ({
   isDecrementing: state.counter.isDecrementing
 })
 
-const mapDispatchToProps = dispatch => bindActionCreators({
+const mapDispatchToProps = dispatch => ({
+  componentDidMount:() => firstLoad(),
+  ...bindActionCreators({
   increment,
   incrementAsync,
   decrement,
   decrementAsync,
   changePage: () => push('/about-us')
-}, dispatch)
+}, dispatch)})
 
-export default connect(
+export default connectWithLifecycle(
   mapStateToProps,
   mapDispatchToProps
 )(Home)
